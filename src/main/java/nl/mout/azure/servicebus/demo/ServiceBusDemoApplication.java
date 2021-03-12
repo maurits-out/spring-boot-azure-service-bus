11b136245074f84e05c299f75032f2c7817b81dc
maurits <17969131+maurits-out@users.noreply.github.com>
2021-03-12 21:19:16 +0100

    Initial project setup.

---
+++ src/main/java/nl/mout/azure/servicebus/demo/ServiceBusDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.mout.azure.servicebus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mout/azure/servicebus/demo/ServiceBusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mout.azure.servicebus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
